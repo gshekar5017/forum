76f8dc3748933538de800efa464c6fed212300ec
gshekar5017 <shekargudepu@gmail.com>
2016-11-10 17:06:03 +0000

    Added Associations between User and Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
  before_action :find_post, only: [:show, :edit, :update,:destroy]
+ before_filter :authenticate_user!, except: [:show, :index]
   def index
     @posts = Post.all.order('created_at DESC')
   end
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
@@ -35,7 +36,7 @@ class PostsController < ApplicationController
   end
   private
    def post_params
-     params.require(:post).permit(:title,:content)
+     params.require(:post).permit(:title,:content, :user_id)
    end
    def find_post
      @post = Post.find(params[:id])
